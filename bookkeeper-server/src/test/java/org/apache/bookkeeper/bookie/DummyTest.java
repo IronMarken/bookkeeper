4b7a6c3767cddad913094f7dbe92b32ac11cf921
ironmarken <ferrimarco98@gmail.com>
2023-05-19 14:12:29 +0200

    dummy test

---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/DummyTest.java
@@ -0,0 +1,13 @@
+package org.apache.bookkeeper.bookie;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DummyTest {
+
+    @Test
+    public void assertTest() {
+        Assert.assertTrue(true);
+    }
+
+}
